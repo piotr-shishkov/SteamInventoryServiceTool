d7f2d748e3a1bf9201801cc9014aca63858fa4f0
Piotr Shishkov <voltshishkov@icloud.com>
2024-06-25 11:07:06 +0300

    - add tags field

---
+++ SteamInventoryServiceTool/Data/Steam/Fields/Tags.cs
@@ -0,0 +1,70 @@
+﻿using Newtonsoft.Json;
+using SteamInventoryServiceTool.Data.Steam.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamInventoryServiceTool.Data.Steam.Fields
+{
+    public class Tags
+    {
+        public Dictionary<string, string> TagsDict { get; set; } = new Dictionary<string, string>();
+    }
+
+    public class TagsJsonConverter : JsonConverter<Tags>
+    {
+        public override Tags? ReadJson(JsonReader reader, Type objectType, Tags? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            var jsonString = reader.Value as string;
+            if (jsonString == null)
+            {
+                return new Tags();
+            }
+
+            var tagsDict = new Dictionary<string, string>();
+            // Separating all tags
+            var separatedTags = jsonString.Split(';');
+            foreach (var tagPair in separatedTags)
+            {
+                // Separate single item by item and its count
+                var split = tagPair.Split(':');
+                var tag = split[0];
+                var tagValue = split[1];
+
+                tagsDict.Add(tag, tagValue);                
+            }
+
+            return new Tags()
+            {
+                TagsDict = tagsDict
+            };
+        }
+
+        public override void WriteJson(JsonWriter writer, Tags? value, JsonSerializer serializer)
+        {
+            var jsonString = string.Empty;
+            if (value == null)
+            {
+                writer.WriteNull();
+                return;
+            }
+
+            var i = 0;
+            foreach (var (tag, tagValue) in value.TagsDict)
+            {
+                jsonString += tag;
+                jsonString += $":{tagValue}";
+
+                // Check if not last item
+                if (i++ < value.TagsDict.Count - 1)
+                {
+                    jsonString += ";";
+                }
+            }
+            writer.WriteValue(jsonString);
+        }
+    }
+    }
+}
