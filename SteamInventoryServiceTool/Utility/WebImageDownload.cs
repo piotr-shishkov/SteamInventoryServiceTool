8c56fcef11a9957c5a86ef0963ced7a53d8ea007
Piotr Shishkov <voltshishkov@icloud.com>
2024-06-25 14:55:28 +0300

    - add web image download utility

---
+++ SteamInventoryServiceTool/Utility/WebImageDownload.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace SteamInventoryServiceTool.Utility;
+
+public static class WebImageDownload
+{
+    public static async Task<BitmapImage> Get(string imageUrl)
+    {
+        using (var client = new HttpClient())
+        {
+            try
+            {
+                var response = await client.GetAsync(imageUrl);
+                var imageStream = await response.Content.ReadAsStreamAsync();
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = imageStream;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+                return bitmapImage;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
