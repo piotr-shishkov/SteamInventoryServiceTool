50cc0e97adbca0108d8512d01c035b58f0d54d95
Piotr Shishkov <voltshishkov@icloud.com>
2024-06-29 13:32:46 +0300

    - update welcome dialog

--- SteamInventoryServiceTool/Windows/Dialogs/WelcomeDialogWindow.xaml.cs
+++ SteamInventoryServiceTool/Windows/Dialogs/WelcomeDialogWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Navigation;
 using SteamInventoryServiceTool.Workspaces;
 
 namespace SteamInventoryServiceTool.Windows.Dialogs;
@@ -15,6 +18,7 @@ public partial class WelcomeDialogWindow : Window
         OpenButton.Click += OpenWorkspace;
         NewButton.Click += NewWorkspace;
         ExitButton.Click += ExitTool;
+        GithubHyperLink.RequestNavigate += RequestNavigate;
     }
 
     private void OpenWorkspace(object sender, RoutedEventArgs e)
@@ -33,5 +37,21 @@ public partial class WelcomeDialogWindow : Window
     private void ExitTool(object sender, RoutedEventArgs e)
     {
         Application.Current.Shutdown();
+    }
+
+    private void RequestNavigate(object sender, RequestNavigateEventArgs e)
+    {
+        try
+        {
+            var url = e.Uri.AbsoluteUri;
+            var processInfo = new ProcessStartInfo(url);
+            processInfo.UseShellExecute = true;
+            Process.Start(processInfo);
+            e.Handled = true; 
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Unable to open link: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+        }   
     }
 }
